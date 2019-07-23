c799b779d3dd465ba8a6b433b19870830b9c21eb
Dario <dkemenovic@gmail.com>
2019-07-23 10:07:48 +0200

    Import new component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from './Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <h1>hello</h1>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </nav>
+      <main>
+        <p>this is where my content will go</p>
+      </main>
+      <Footer />  
     </div>
-  );
+  )
 }
 
 export default App;
