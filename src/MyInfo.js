4abff50ddb9b1dff8e793209a492699822b6e82a
Dario <dkemenovic@gmail.com>
2019-07-23 08:44:18 +0200

    Create new file

---
+++ src/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+	return (
+		<div>
+			<h1>Dario</h1>
+			<p>Codieren lernen</p>
+			<ul>
+				<li>Germany</li>
+				<li>Japan</li>
+				<li>China</li>
+			</ul>
+		</div>
+	)
+}
+
+export default MyInfo;
