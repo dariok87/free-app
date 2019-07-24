17742856e6d063cd67aeb47154c77d3e6a00fe84
Dario <dkemenovic@gmail.com>
2019-07-24 18:30:37 +0200

    Add component

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ToDoItem = () => 
+    <div className="to-do-item">
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+
+export default ToDoItem;
