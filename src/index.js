1b5a158bb78ae7481c0bb46c7f3fe0664334a0bb
Dario <dkemenovic@gmail.com>
2019-07-24 18:50:43 +0200

    Import app and css file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
-import MyInfo from './MyInfo';
+import App from './App';
+import './style.css';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-	<MyInfo />,
- document.getElementById('root'));
+/* function App () {
+	const date = new Date(2019, 7, 23, 20);
+	const hours = date.getHours();
+	let timeOfDay;
+	const styles = {
+		backgroundColor: "yellow",
+		fontSize: "43px"
+	}
 
- serviceWorker.unregister();
+if (hours < 12) {
+	timeOfDay = "morning"
+} else if (hours >= 12 && hours < 17) {
+	timeOfDay = "afternoon"
+} else {
+	timeOfDay = "night";
+	styles.backgroundColor = "purple";
+}
+
+	return (
+		<h1 style={styles}>Good {timeOfDay}</h1>
+	)
+} */
+
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+);
+
+serviceWorker.unregister();
