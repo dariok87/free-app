9702cb2e7b351dcd085acb9916bd6dd71983b209
Dario <dkemenovic@gmail.com>
2019-07-26 19:48:44 +0200

    Rename file

---
+++ src/todosData.js
@@ -0,0 +1,24 @@
+const ToDosData = [
+	{
+		id: 1,
+		text: "Take out the trash",
+		completed: true
+	},
+	{
+		id: 2,
+		text: "Move out",
+		completed: false
+	},
+	{
+		id: 3,
+		text: "Get in",
+		completed: true
+	},
+	{
+		id: 4,
+		text: "Here you are",
+		completed: false
+	}
+]
+
+export default ToDosData;
