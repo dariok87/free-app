57d3f67dc3b4101d8241e7cfd80d3c26176ff97a
Dario <dkemenovic@gmail.com>
2019-10-03 20:11:06 +0200

    Add state

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TodoItem(props) {
   return (
     <div className="to-do-item">
-      <input type="checkbox" checked={props.item.completed}/>
+      <input type="checkbox" checked={props.item.completed} onChange={() => console.log("changing")}/>
       <p>{props.item.text}</p>
     </div>
   )
