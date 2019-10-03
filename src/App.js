af4711a2af90fc5051de5ebbf831ca3ef06173fe
Dario <dkemenovic@gmail.com>
2019-10-03 20:22:03 +0200

    Create Task

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+/*
+Create event handler in the App component for when the checkbox is clicked('which is an 'onChange event')
+Pass the method down to the TodoItem component.
+In the TodoItem Component make it so when the 'onChange event happens , it calls the 'handleChange' method and passes the id of the todo into the function
+*/
+
 import React from 'react';
 import TodoItem from './TodoItem';
 import todoData from './todoData';
@@ -8,17 +14,11 @@ class App extends React.Component  {
     this.state = {
       todo: todoData
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  render() {
-    const todoItems = this.state.todo.map(item => <TodoItem key={item.id} item={item} />)
-
-    return (
-      <div className="to-do-list">
-        {todoItems}
-      </div>
-    )
+  handleChange(id) {
+    //update state so that one item with the given id flips 'completed' from false to true. Do not modify prevState directly
   }
-}
 
 export default App;
