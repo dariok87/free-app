41bd2810318f9a38a084877ef4678e41fbfb4483
Dario <dkemenovic@gmail.com>
2019-10-03 20:26:07 +0200

    Refactor code

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,11 @@ import React from 'react';
 function TodoItem(props) {
   return (
     <div className="to-do-item">
-      <input type="checkbox" checked={props.item.completed} onChange={() => console.log("changing")}/>
+      <input 
+        type="checkbox"
+        checked={props.item.completed} 
+        onChange={() => console.log("changing")}
+      />
       <p>{props.item.text}</p>
     </div>
   )
