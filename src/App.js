4fe4a03f1d26fceac18ed0978abb8b7f69fb6917
Dario <dkemenovic@gmail.com>
2019-10-03 21:56:34 +0200

    Pass method to the component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
-Create event handler in the App component for when the checkbox is clicked('which is an 'onChange event')
-Pass the method down to the TodoItem component.
-In the TodoItem Component make it so when the 'onChange event happens , it calls the 'handleChange' method and passes the id of the todo into the function
+1. Create event handler in the App component for when the checkbox is clicked('which is an 'onChange event')
+2. Pass the method down to the TodoItem component.
+3. In the TodoItem Component make it so when the 'onChange event happens , it calls the 'handleChange' method and passes the id of the todo into the function
 */
 
 import React from 'react';
@@ -18,11 +18,11 @@ class App extends React.Component  {
   }
 
   handleChange(id) {
-
+    console.log("Changed", id)
   }
 
   render() {
-    const todoItems = this.state.todo.map(item => <TodoItem key={item.id} item={item} />)
+    const todoItems = this.state.todo.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
 
     return (
       <div className="to-do-list">
