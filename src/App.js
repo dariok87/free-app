121e9d8fc8c681392a477277c862211913279411
Dario <dkemenovic@gmail.com>
2019-10-04 23:26:56 +0200

    Update state with map method

--- src/App.js
+++ src/App.js
@@ -18,7 +18,17 @@ class App extends React.Component  {
   }
 
   handleChange(id) {
-    console.log("Changed", id)
+    this.setState(prevState => {
+      const updatedTodo = prevState.todo.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todo: updatedTodo
+      }
+    })
   }
 
   render() {
